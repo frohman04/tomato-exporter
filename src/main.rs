10bf578efa00aeb1c875e68dadddfeab247287ff
Chris Lieb <chris.lieb@gmail.com>
2020-08-03 11:59:28 -0400

    Retrieve and parse bandwidth numbers from router

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,29 @@
+extern crate regex;
+extern crate reqwest;
+extern crate serde_json;
 extern crate serde_yaml;
 
+#[cfg(test)]
+#[macro_use]
+extern crate maplit;
+
+mod bandwidth;
 mod config;
 
 fn main() {
     let conf = config::load_conf();
     println!("{:?}", conf);
+    let client = conf
+        .modules
+        .mod_bandwidth
+        .map(|c| {
+            bandwidth::BandwidthClient::new(
+                c.router_ip,
+                c.admin_username,
+                c.admin_password,
+                c.http_id,
+            )
+        })
+        .expect("Must define mod_bandwidth configuration");
+    println!("{:?}", client.get_bandwidth());
 }
