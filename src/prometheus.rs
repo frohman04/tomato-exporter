22e56f9be7134914de16f52bb6133d8a094f6eda
Chris Lieb <chris.lieb@gmail.com>
2020-08-05 20:54:11 -0400

    Convert the objects into Prometheus wire format

--- src/prometheus.rs
+++ src/prometheus.rs
@@ -49,9 +49,8 @@ pub async fn metrics(data: web::Data<WebState>) -> Result<String, error::Error> 
             .flatten()
             .collect(),
     )]);
-    info!("{:?}", resp);
 
-    Ok("hello world".to_string())
+    Ok(resp.to_string())
 }
 
 #[derive(PartialEq, PartialOrd, Debug)]
@@ -62,6 +61,14 @@ struct PromResponse {
 impl PromResponse {
     pub fn new(metrics: Vec<PromMetric>) -> PromResponse {
         PromResponse { metrics }
+    }
+
+    pub fn to_string(&self) -> String {
+        self.metrics
+            .iter()
+            .map(|metric| metric.to_string())
+            .collect::<Vec<String>>()
+            .join("\n")
     }
 }
 
@@ -97,6 +104,21 @@ impl PromMetric {
             samples,
         }
     }
+
+    pub fn to_string(&self) -> String {
+        format!(
+            "# HELP {} {}\n# TYPE {} {}\n{}",
+            self.name,
+            self.help,
+            self.name,
+            format!("{:?}", self.typ).to_lowercase(),
+            self.samples
+                .iter()
+                .map(|sample| sample.to_string(self.name.clone()))
+                .collect::<Vec<String>>()
+                .join("\n")
+        )
+    }
 }
 
 #[derive(PartialEq, PartialOrd, Debug)]
@@ -114,6 +136,21 @@ impl PromSample {
             timestamp,
         }
     }
+
+    pub fn to_string(&self, name: String) -> String {
+        format!(
+            "{}{{{}}} {}{}",
+            name,
+            self.labels
+                .iter()
+                .map(|label| label.to_string())
+                .collect::<Vec<String>>()
+                .join(","),
+            self.value.to_string(),
+            self.timestamp
+                .map_or_else(|| "".to_string(), |ts| format!(" {}", ts.to_string()))
+        )
+    }
 }
 
 #[derive(Eq, PartialEq, PartialOrd, Debug)]
@@ -128,5 +165,153 @@ impl PromLabel {
             name: name.to_string(),
             value,
         }
+    }
+
+    pub fn to_string(&self) -> String {
+        format!("{}=\"{}\"", self.name, self.value)
+    }
+}
+
+#[cfg(test)]
+#[allow(non_snake_case)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test__PromLabel__to_string() {
+        let label = PromLabel::new("foo", "bar".to_string());
+        assert_eq!(label.to_string(), "foo=\"bar\"")
+    }
+
+    #[test]
+    fn test__PromSample__to_string__no_labels_no_timestamp() {
+        let sample = PromSample::new(vec![], 4.5, None);
+        assert_eq!(sample.to_string("baz".to_string()), "baz{} 4.5")
+    }
+
+    #[test]
+    fn test__PromSample__to_string__no_labels_with_timestamp() {
+        let sample = PromSample::new(vec![], 4.5, Some(12345));
+        assert_eq!(sample.to_string("baz".to_string()), "baz{} 4.5 12345")
+    }
+
+    #[test]
+    fn test__PromSample__to_string__one_label_no_timestamp() {
+        let sample = PromSample::new(vec![PromLabel::new("foo", "bar".to_string())], 4.5, None);
+        assert_eq!(sample.to_string("baz".to_string()), "baz{foo=\"bar\"} 4.5")
+    }
+
+    #[test]
+    fn test__PromSample__to_string__many_labels_no_timestamp() {
+        let sample = PromSample::new(
+            vec![
+                PromLabel::new("foo", "bar".to_string()),
+                PromLabel::new("go", "bucks".to_string()),
+            ],
+            4.5,
+            None,
+        );
+        assert_eq!(
+            sample.to_string("baz".to_string()),
+            "baz{foo=\"bar\",go=\"bucks\"} 4.5"
+        )
+    }
+
+    #[test]
+    fn test__PromMetric__to_string__no_samples() {
+        let metric = PromMetric::new("baz", "A funny value", PromMetricType::Counter, vec![]);
+        assert_eq!(
+            metric.to_string(),
+            "# HELP baz A funny value\n# TYPE baz counter\n"
+        )
+    }
+
+    #[test]
+    fn test__PromMetric__to_string__one_sample() {
+        let metric = PromMetric::new(
+            "baz",
+            "A funny value",
+            PromMetricType::Counter,
+            vec![PromSample::new(
+                vec![PromLabel::new("foo", "bar".to_string())],
+                4.5,
+                None,
+            )],
+        );
+        assert_eq!(
+            metric.to_string(),
+            "# HELP baz A funny value\n# TYPE baz counter\nbaz{foo=\"bar\"} 4.5"
+        )
+    }
+
+    #[test]
+    fn test__PromMetric__to_string__many_samples() {
+        let metric = PromMetric::new(
+            "baz",
+            "A funny value",
+            PromMetricType::Counter,
+            vec![
+                PromSample::new(vec![PromLabel::new("foo", "bar".to_string())], 4.5, None),
+                PromSample::new(vec![], 4.5, Some(12345)),
+            ],
+        );
+        assert_eq!(
+            metric.to_string(),
+            "# HELP baz A funny value\n# TYPE baz counter\nbaz{foo=\"bar\"} 4.5\nbaz{} 4.5 12345"
+        )
+    }
+
+    #[test]
+    fn test__PromResponse__to_string__no_metrics() {
+        let response = PromResponse::new(vec![]);
+        assert_eq!(response.to_string(), "")
+    }
+
+    #[test]
+    fn test__PromResponse__to_string__one_metric() {
+        let response = PromResponse::new(vec![PromMetric::new(
+            "baz",
+            "A funny value",
+            PromMetricType::Counter,
+            vec![PromSample::new(
+                vec![PromLabel::new("foo", "bar".to_string())],
+                4.5,
+                None,
+            )],
+        )]);
+        assert_eq!(
+            response.to_string(),
+            "# HELP baz A funny value\n# TYPE baz counter\nbaz{foo=\"bar\"} 4.5"
+        )
+    }
+
+    #[test]
+    fn test__PromResponse__to_string__many_metrics() {
+        let response = PromResponse::new(vec![
+            PromMetric::new(
+                "baz",
+                "A funny value",
+                PromMetricType::Counter,
+                vec![PromSample::new(
+                    vec![PromLabel::new("foo", "bar".to_string())],
+                    4.5,
+                    None,
+                )],
+            ),
+            PromMetric::new(
+                "spam",
+                "A silly value",
+                PromMetricType::Counter,
+                vec![PromSample::new(
+                    vec![PromLabel::new("bar", "foo".to_string())],
+                    5.4,
+                    None,
+                )],
+            ),
+        ]);
+        assert_eq!(
+            response.to_string(),
+            "# HELP baz A funny value\n# TYPE baz counter\nbaz{foo=\"bar\"} 4.5\n# HELP spam A silly value\n# TYPE spam counter\nspam{bar=\"foo\"} 5.4"
+        )
     }
 }
