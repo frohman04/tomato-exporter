739c5ee1e37398009da989a0ce0e3f63c8b7c4a9
frohman04 <frohman04@users.noreply.github.com>
2024-03-27 09:20:11 -0400

    Update release script

--- release.sh
+++ release.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
 
 set -e
-
-#export ARMV7_UNKNOWN_LINUX_GNUEABIHF_OPENSSL_LIB_DIR=/c/SysGCC/raspberry/arm-linux-gnueabihf/sysroot/usr/lib/arm-linux-gnueabihf
-#export ARMV7_UNKNOWN_LINUX_GNUEABIHF_OPENSSL_INCLUDE_DIR=/c/SysGCC/raspberry/arm-linux-gnueabihf/sysroot/usr/include/openssl
-export PATH=/c/SysGCC/raspberry/bin:/c/SysGCC/raspberry/arm-linux-gnueabihf/bin:${PATH}
-
 set -x
 
 cargo test
 cargo zigbuild --release --target aarch64-unknown-linux-gnu
-scp conf.yaml target/aarch64-unknown-linux-gnu/release/tomato-exporter gilneas:~
-ssh gilneas -- "chmod +x ~/tomato-exporter && sudo mv ~/tomato-exporter /usr/local/bin/tomato_exporter && sudo mv ~/conf.yaml /etc/tomato-exporter/conf.yaml && sudo systemctl restart tomato_exporter"
+scp conf.json target/aarch64-unknown-linux-gnu/release/tomato-exporter gilneas:~
+ssh gilneas -- "chmod +x ~/tomato-exporter && sudo mv ~/tomato-exporter /usr/local/bin/tomato_exporter && sudo mv ~/conf.json /etc/tomato-exporter/conf.json && sudo systemctl restart tomato_exporter"
