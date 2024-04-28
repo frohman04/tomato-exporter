554673260323a1c27cec7a288ec0690dc934c6f5
frohman04 <frohman04@users.noreply.github.com>
2024-04-28 14:09:09 -0400

    Update build script for WSL

--- release.sh
+++ release.sh
@@ -4,6 +4,12 @@ set -e
 set -x
 
 cargo test
-cargo zigbuild --release --target aarch64-unknown-linux-gnu
+if [ -z ${OS+x} ]; then
+    BUILD=build
+else
+    # in Windows, so need to uze zigbuild for aarch64 compat
+    BUILD=zigbuild
+fi
+cargo ${BUILD} --release --target aarch64-unknown-linux-gnu
 scp conf.json target/aarch64-unknown-linux-gnu/release/tomato-exporter gilneas:~
 ssh gilneas -- "chmod +x ~/tomato-exporter && sudo mv ~/tomato-exporter /usr/local/bin/tomato_exporter && sudo mv ~/conf.json /etc/tomato-exporter/conf.json && sudo systemctl restart tomato_exporter"
